d1906ffb23bcc29b9568cea5a891418919a2cdd6
kevingallagher34 <kevingallagher34@users.noreply.github.com>
2025-12-27 22:03:53 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('simplellm is running');
+app.get("/", (req, res) => {
+  res.send("simplellm is running");
+});
+
+app.post("/chat", (req, res) => {
+  const { message } = req.body;
+
+  res.json({
+    reply: `You said: ${message}`
+  });
 });
 
 const PORT = process.env.PORT || 3000;
