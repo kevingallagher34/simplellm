c8413a24f8294e8087f5db631c82fba54b83ae7c
Gallagher <gallagher-kevin@aramark.com>
2025-12-29 21:40:55 -0500

    fix bugs in brackets

--- index.js
+++ index.js
@@ -159,8 +159,7 @@ app.post("/speak", async (req, res) => {
 });
 */
 
-
-
+// Start server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
